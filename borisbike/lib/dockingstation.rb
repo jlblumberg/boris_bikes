9a0a9176f12d13328cd2bd76197f70512020f70a
Haydon Houghton <haydonhoughton@outlook.com>
2020-01-22 17:52:11 +0000

    pairing progress chapter 11

--- borisbike/lib/dockingstation.rb
+++ borisbike/lib/dockingstation.rb
@@ -1,2 +1,18 @@
+require_relative 'bike'
+
 class DockingStation
+
+    attr_reader :bike
+
+    def release_bike
+        released = Bike.new
+        return released
+    end
+
+    def dock(bike)
+        @bike = bike 
+        return bike
+    end     
+
+
 end
--- borisbike/spec/docking_station_spec.rb
+++ borisbike/spec/docking_station_spec.rb
@@ -1,4 +1,26 @@
 require 'dockingstation'
 
-describe DockingStation do 
-end 
+describe DockingStation do
+
+    subject(:station) {described_class.new}
+
+    it { is_expected.to respond_to(:release_bike) }
+
+    it "should release a working bike" do
+        expect(station.release_bike.working?).to eq true  
+    end
+
+    it 'should dock a bike' do
+        bike = Bike.new 
+        expect(station.dock(bike)).to eq bike
+    end    
+
+    it 'should return the docked bike when asked' do
+        bike = Bike.new
+        station.dock(bike) 
+        expect(station.bike).to eq bike 
+    end     
+
+end
+
+
