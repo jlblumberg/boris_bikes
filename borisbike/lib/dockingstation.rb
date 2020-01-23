ec79cab0954debf2e557e34cdd80b4964713a5cb
Megan Hayes <student@Makerss-MacBook-Air.local>
2020-01-23 16:19:25 +0000

    raise error progress

--- borisbike/lib/dockingstation.rb
+++ borisbike/lib/dockingstation.rb
@@ -4,14 +4,26 @@ class DockingStation
 
     attr_reader :bike
 
+    available_bikes = 1
+
     def release_bike
-        released = Bike.new
-        return released
+      released = Bike.new
+      available_bikes = available_bikes - 1 
+      if available_bikes < 1
+    raise "message"
+      else 
+        return released 
+
+      end
+    
     end
+
 
     def dock(bike)
         @bike = bike 
     end     
+
+
 
 
 end
