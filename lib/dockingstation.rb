18e21a7e007cb405b48330a2172a6fc2e3bb597c
Josh Blumberg <jlblumberg@gmail.com>
2020-01-24 16:49:20 +0000

    finish step 13. limited capacity

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -10,16 +10,20 @@ class DockingStation
 
     def release_bike
       if @available_bikes == 0
-        raise "message"
+        raise "No bikes available"
       else 
-        released = Bike.new 
+        bike = Bike.new 
         @available_bikes -= 1
-        return released 
+        return bike 
      end
     end
 
-    def dock(bike)
-      @bike = bike 
+    def dock(bike) 
+      if @available_bikes == 1
+        raise "Cant dock. Station is full"
+      else
+        @bike = bike
+      end
     end     
 
 
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -12,21 +12,26 @@ describe DockingStation do
 
     it 'should dock a bike' do
         bike = Bike.new 
+        station.release_bike
         expect(station.dock(bike)).to eq bike
     end    
 
-    it 'should return the docked bike when asked' do
+    it 'should return the docked bike when a bike is docked' do
         bike = Bike.new
-        station.dock(bike) 
-        expect(station.bike).to eq bike 
+        station.release_bike
+        expect(station.dock(bike)).to eq bike 
     end     
 
     it 'should return error when asked for a bike when there is no bike' do 
       # first do release bike so that available bikes goes to 0
       station.release_bike
-      expect {station.release_bike}.to raise_error("message")
+      expect {station.release_bike}.to raise_error("No bikes available")
     end    
 
+    it 'should reject docking if theres already a bike in the dock' do
+      bike = Bike.new
+      expect {station.dock(bike)}.to raise_error("Cant dock. Station is full")
+    end
 end
 
 
